2caa9857fef2a1d876f88928bd5983ebd65276c4
Jonathan Ho <jonafanho@gmail.com>
2024-03-12 13:05:32 -0700

    Fixed build script not downloading libraries to the right directory

--- buildSrc/src/main/java/org/mtr/core/WebserverSetup.java
+++ buildSrc/src/main/java/org/mtr/core/WebserverSetup.java
@@ -19,11 +19,13 @@ public class WebserverSetup {
 
 	private static final Logger LOGGER = LogManager.getLogger("WebserverSetup");
 
-	public static void setup(File projectPath) {
-		final Path mapPath = projectPath.toPath().resolve("buildSrc/src/main/resources/website/src/app/map");
+	public static void download(File projectPath) {
+		final Path mapPath = projectPath.toPath().resolve("buildSrc/src/main/resources/website/src/app/component/map");
 		write(mapPath.resolve("three.module.min.js"), download("https://raw.githubusercontent.com/mrdoob/three.js/dev/build/three.module.min.js"));
 		write(mapPath.resolve("BufferGeometryUtils.js"), download("https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/jsm/utils/BufferGeometryUtils.js").replace("from 'three'", "from './three.module.min.js'"));
+	}
 
+	public static void setup(File projectPath) {
 		final Path websitePath = projectPath.toPath().resolve("buildSrc/src/main/resources/website/dist/website/browser");
 		final StringBuilder stringBuilder = new StringBuilder("package org.mtr.core.generated;@javax.annotation.Nullable public final class WebserverResources{public static String get(String resource){switch(resource.startsWith(\"/\")?resource.substring(1):resource){");
 		try (final Stream<Path> stream = Files.list(websitePath)) {
