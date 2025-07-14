11a53723ea58e3f588f36dcd778dc23668563767
Jonathan Ho <jonafanho@gmail.com>
2025-07-14 14:05:42 -0700

    Fix imports

--- src/main/java/org/mtr/core/operation/BlockRails.java
+++ src/main/java/org/mtr/core/operation/BlockRails.java
@@ -1,11 +1,11 @@
 package org.mtr.core.operation;
 
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import org.mtr.core.data.Rail;
 import org.mtr.core.generated.operation.BlockRailsSchema;
 import org.mtr.core.serializer.ReaderBase;
 import org.mtr.core.simulation.Simulator;
-import org.mtr.libraries.it.unimi.dsi.fastutil.ints.IntArrayList;
-import org.mtr.libraries.it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 public final class BlockRails extends BlockRailsSchema {
 
